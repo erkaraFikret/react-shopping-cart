3232b8fb9f3aeb26d9a6ae910c0bcb43b97c089d
Fikret Erkara <fikreterkara_26@hotmail.com>
2024-12-23 22:05:55 +0300

    created Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
+import { allProducts } from './assets/data'
+import CartItem from './components/CartItem'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <h1 className="grid place-items-center py-20">
+      <h1 className="text-5xl italic text-gray-500 mb-16">
+        Fashion Buzz: Top Trends to Elevate Your Look
+      </h1>
+      <div className='grid grid-cols-3 place-items-start gap-10'>
+        {allProducts?.map((item) => {
+          return (
+            <CartItem key={item.id} item={item} />
+          )
+        })}
+      </div>
+    </h1>
   )
 }
 
